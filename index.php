ffdb3f5a8a9cf9280d98b0d12d49da275dddbca7
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2025-02-02 00:42:43 +0600

    learnibg
    php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hallo world" . '<br>';
+echo phpversion();
+
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/* This is a
+multi-line comment */
+echo "<br>";
+$txt = "W3Schools.com";
+echo "I love $txt!" . '<br>';
+//When we concatanate with variable
+$txt1 = "W3Schools.com";
+echo "I love " . $txt1 . "!";
+echo "<br>";
+//To show data type & value
+$x = 5;
+var_dump($x);
